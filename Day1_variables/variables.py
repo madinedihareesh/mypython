0452231e96931fdb2f2616aeabf297f14af2fcb0
Hareesh <hareesh.madinedi@gmail.com>
2025-05-23 09:23:35 +0530

    Variables

---
+++ Day1_variables/variables.py
@@ -0,0 +1,33 @@
+#python is a dinamically typed language
+import keyword
+'''
+Limitation of variables:
+1.It should contain alphanumaric and an '_'.
+2.It should not strat with numbers.
+3.No special keyword should be allocated for a varible.
+4.NO special charecters should be used.
+'''
+name1='Ganesh'
+Name1="Bala-Ganesh"
+name_1='Surya'
+_name1='Harini'
+
+print(name1)
+print(name_1)
+print(Name1)
+print(_name1)
+print(keyword.kwlist)
+print(len(keyword.kwlist))
+
+#Data Types
+'''
+1. numaric
+   a.int (numbers)
+   b.flot (decimal)
+   c.boolen (True or False)
+   d.complex ()
+'''
+
+
+
+
