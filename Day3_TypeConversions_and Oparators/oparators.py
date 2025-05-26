91b38b71cddd3f6abc9264240ae0ae35e5d3354c
Hareesh <hareesh.madinedi@gmail.com>
2025-05-26 22:11:59 +0530

    Day4_TypeConversions

---
+++ Day3_TypeConversions_and Oparators/oparators.py
@@ -0,0 +1,20 @@
+'''
+Arthamatic
+1.+(add)
+2.-(sub)
+3.*(mul)
+4./(flot div)
+5.%(reminder)
+6.//(floor div)
+7.**(power)
+'''
+# expression
+
+a,b=20,30
+print("Addition of two values",a+b)
+print("Sub of two values",a-b)
+print("Mul of two values",a*b)
+print("flot div of two values",a/b)
+print("reminder of two values",a%b)
+print("floor div of two values",a//b)
+print('Expo of two values',a**b)
---
+++ Day3_TypeConversions_and Oparators/typeconversion.py
@@ -0,0 +1,43 @@
+'''
+int
+flot,
+complex,
+boolen,
+string
+'''
+#integer type conversiion
+number=10
+print(float(number))
+print(complex(number))
+print(bool())
+print(type(str(number)))
+#float Type conversion
+f=1257E-2
+print(f)
+print(int(f))
+print(complex(f))
+print(str(f))
+#complex
+c=complex(12,9)
+print(c)
+# print(int(c))
+# print(float(c))
+print(bool(c))
+print(str(c))
+# String
+print(type(str(10)))
+print(str(12.57))
+print(str(12+9j))
+print(str(True))
+#int
+print(int(f))
+print(int('10'))
+print(int(True))
+# print(int(12+9j))
+
+e=20
+print(id(e))
+e=30
+print(id(e))
+e='BalaGanesh'
+print(id(e))
