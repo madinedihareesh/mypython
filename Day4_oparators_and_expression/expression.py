d236ef653a3634d2bf281c6b6b80624d95b9d6f8
Hareesh <hareesh.madinedi@gmail.com>
2025-05-27 19:00:00 +0530

    Day-4

---
+++ Day4_oparators_and_expression/expression.py
@@ -0,0 +1,26 @@
+import math
+# base=int(input("Enter the base Value"))
+# height=int(input("Enter the height value"))
+
+# areaOfTriangle=(base*height)/2
+# print(areaOfTriangle)
+
+
+
+# side=int(input("Enter the side value")) 
+# areaofS=side**2
+# print(areaofS)
+
+# length=int(input("Enter the length"))
+# breth=int(input("Enter the breth"))
+# print(length*breth)
+
+# radius=int(input("Enter the Radious"))
+# print(math.pi(radius**2))
+# it is 
+a=int(input("Enter the value of a")) 
+b=int(input("Enter the value of b"))
+print((a+b)**2)
+
+x=(-b+math.sqrt(b**2-(4*a*c)))/2*a
+x1=(-b-math.sqrt(b**2-(4*a*c)))/2*a
